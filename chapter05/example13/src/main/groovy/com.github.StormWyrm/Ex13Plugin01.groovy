5e4f285566b2b8e6fce959fcdccfe17ea73dac16
liqingfeng <fengfeng@lonlife.cn>
2018-12-13 10:54:14 +0800

    自定Gradle插件并发布到远程仓库

--- chapter05/example13/src/main/groovy/com.github.StormWyrm/Ex13Plugin01.groovy
+++ chapter05/example13/src/main/groovy/com.github.StormWyrm/Ex13Plugin01.groovy
@@ -1,4 +1,14 @@
 package com.github.StormWyrm
 
-class Ex13Plugin01 {
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class Ex13Plugin01 implements Plugin<Project> {
+
+    @Override
+    void apply(Project target) {
+        target.task("ex13Task01")<<{
+            println '这是通过groovy项目中自定义的插件中自定以的任务'
+        }
+    }
 }
