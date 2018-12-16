be67a369dc6520d3d8e062b1f5cb8865a621db3e
liqingfeng <fengfeng@lonlife.cn>
2018-12-16 19:28:18 +0800

    动态添加自定义资源

--- chapter07/example19/build/generated/not_namespaced_r_class_sources/baiduDebug/processBaiduDebugResources/r/android/support/design/R.java
+++ chapter07/example19/build/generated/not_namespaced_r_class_sources/baiduDebug/processBaiduDebugResources/r/android/support/design/R.java
@@ -1283,19 +1283,19 @@ public final class R {
         public static final int abc_toolbar_collapse_description = 0x7f0c0026;
         public static final int appbar_scrolling_view_behavior = 0x7f0c0027;
         public static final int bottom_sheet_behavior = 0x7f0c0028;
-        public static final int character_counter_content_description = 0x7f0c0029;
-        public static final int character_counter_pattern = 0x7f0c002a;
-        public static final int fab_transformation_scrim_behavior = 0x7f0c002b;
-        public static final int fab_transformation_sheet_behavior = 0x7f0c002c;
-        public static final int hide_bottom_view_on_scroll_behavior = 0x7f0c002d;
-        public static final int mtrl_chip_close_icon_content_description = 0x7f0c002e;
-        public static final int password_toggle_content_description = 0x7f0c002f;
-        public static final int path_password_eye = 0x7f0c0030;
-        public static final int path_password_eye_mask_strike_through = 0x7f0c0031;
-        public static final int path_password_eye_mask_visible = 0x7f0c0032;
-        public static final int path_password_strike_through = 0x7f0c0033;
-        public static final int search_menu_title = 0x7f0c0034;
-        public static final int status_bar_notification_info_overflow = 0x7f0c0035;
+        public static final int character_counter_content_description = 0x7f0c002a;
+        public static final int character_counter_pattern = 0x7f0c002b;
+        public static final int fab_transformation_scrim_behavior = 0x7f0c002c;
+        public static final int fab_transformation_sheet_behavior = 0x7f0c002d;
+        public static final int hide_bottom_view_on_scroll_behavior = 0x7f0c002e;
+        public static final int mtrl_chip_close_icon_content_description = 0x7f0c002f;
+        public static final int password_toggle_content_description = 0x7f0c0030;
+        public static final int path_password_eye = 0x7f0c0031;
+        public static final int path_password_eye_mask_strike_through = 0x7f0c0032;
+        public static final int path_password_eye_mask_visible = 0x7f0c0033;
+        public static final int path_password_strike_through = 0x7f0c0034;
+        public static final int search_menu_title = 0x7f0c0035;
+        public static final int status_bar_notification_info_overflow = 0x7f0c0036;
     }
     public static final class style {
         private style() {}
